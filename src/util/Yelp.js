ada2814d357da2d74a4b1bb5100cb8e8144fe2c7
browe83 <rowe.brandon.j@gmail.com>
2020-09-23 14:27:45 -0400

    creates, wires up Yelp module

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,34 @@
 const apiKey =
   "59noMRjp_FqU5cmm7d7fWjbn37V5diXMVHT-o_DCpliG1wWrZyzcYierXw9BSWru3JU7grob4EzJOKjHdqWO70YAPW5QQJJmFfqwM8QwWvvw-pRtkL5622FS5l1rX3Yx";
+
+const Yelp = {
+  search: async function (term, location, sortBy) {
+    return fetch(
+      `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+      {
+        headers: { Authorization: `Bearer ${apiKey}` },
+      }
+    )
+      .then((respsonse) => {
+        return respsonse.json();
+      })
+      .then((jsonResponse) => {
+        if (jsonResponse.businesses) {
+          return jsonResponse.businesses.map((business) => ({
+            id: business.id,
+            imageSrc: business.image_url,
+            name: business.name,
+            address: business.location.address1,
+            city: business.location.city,
+            state: business.location.city,
+            zipCode: business.location.zip_code,
+            category: business.categories[0].alias,
+            rating: business.rating,
+            reviewCount: business.review_count,
+          }));
+        }
+      });
+  },
+};
+
+export default Yelp;
